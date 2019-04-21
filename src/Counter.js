78685f9aec710bc22c6d1fb5a3efd75029361230
ayunas <ayunas@gmail.com>
2019-04-21 00:34:58 -0500

    rendered a basic counter to the page using pure react

---
+++ src/Counter.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Counter extends React.Component {
+    constructor() {
+        super();
+
+        this.state = { count : 0 }
+    }
+    
+    increment = () => {
+        this.setState({
+            count : this.state.count + 1
+        })
+    }
+
+    decrement = () => {
+        this.setState({
+            count : this.state.count - 1
+        })
+    }
+    
+    render() {
+        console.log(this.state.count);
+
+        return (
+        <div>
+            <h2>Counter</h2>
+            <div>
+                <button onClick={this.decrement}> - </button>
+                <span>{this.state.count}</span>
+                <button onClick={this.increment}> + </button>
+            </div>
+        </div> 
+        )
+    }
+}
+
+export default Counter;
